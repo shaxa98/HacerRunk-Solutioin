c0211296fcbb15371f8b45bbeab680423cccf20c
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-03-03 18:35:42 +0500

    second

--- fisrt.js
+++ fisrt.js
@@ -4,3 +4,20 @@ function gradingStudents(grades) {
   });
   return finalGrades;
 }
+//int: the length of the longest subarray that meets the criterion
+function pickingNumbers(a) {
+  let max = 0;
+  let values = new Array(100).fill(0);
+
+  (a || []).forEach((value) => {
+    values[value]++;
+  });
+
+  return values.reduce((target, value, index) => {
+    index >= 1 &&
+      value + values[index - 1] > target &&
+      (target = value + values[index - 1]);
+
+    return target;
+  }, []);
+}
