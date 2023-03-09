2867fce46573a23a3bb3bfbb45e7f29dec41d1f4
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-03-09 16:02:15 +0500

    hhhhh

--- fisrt.js
+++ fisrt.js
@@ -1,26 +1,23 @@
-function gradingStudents(grades) {
-  let finalGrades = grades.map((grade) => {
-    return grade >= 38 && grade % 5 >= 3 ? grade - (grade % 5) + 5 : grade;
-  });
-  return finalGrades;
-}
-//int: the length of the longest subarray that meets the criterion
-function pickingNumbers(a) {
-  let max = 0;
-  let values = new Array(100).fill(0);
+// function gradingStudents(grades) {
+//   let finalGrades = grades.map((grade) => {
+//     return grade >= 38 && grade % 5 >= 3 ? grade - (grade % 5) + 5 : grade;
+//   });
+//   return finalGrades;
+// }
+// //int: the length of the longest subarray that meets the criterion
+// function pickingNumbers(a) {
+//   let max = 0;
+//   let values = new Array(100).fill(0);
 
-  (a || []).forEach((value) => {
-    values[value]++;
-  });
+//   (a || []).forEach((value) => {
+//     values[value]++;
+//   });
 
-  return values.reduce((target, value, index) => {
-    index >= 1 &&
-      value + values[index - 1] > target &&
-      (target = value + values[index - 1]);
+//   return values.reduce((target, value, index) => {
+//     index >= 1 &&
+//       value + values[index - 1] > target &&
+//       (target = value + values[index - 1]);
 
-    return target;
-  }, []);
-}
-for (let i = 0; i < 10; i++) {
-  console.log("shaxzod");
-}
+//     return target;
+//   }, []);
+// }
