de7db6e4a23857088bc0545972d89d5e022a32c9
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-03-09 17:17:14 +0500

    add loop

--- fisrt.js
+++ fisrt.js
@@ -23,14 +23,14 @@
 // }
 function makeBox(number) {
   const divbox = '<div class="box"></div> ';
-  console.log(divbox);
+
   let result;
   for (let i = 0; i < number / 2; i++) {
     result = divbox + divbox;
 
     //container[i].innerHTML = result;
+    console.log(result);
   }
-  console.log(result);
 }
 
 makeBox(10);
